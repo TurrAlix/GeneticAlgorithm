ab9ad17556b2e02c8ea001a6f73518f4a0a5cd64
alice01 turrini <alice.turrini@hotmail.com>
2023-11-07 14:28:30 +0100

    Add plot_graph for the convergence graph

--- src/TSP.py
+++ src/TSP.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 import time
 import Utilis
+import matplotlib.pyplot as plt
 
 
 def read_from_file(filename):
@@ -24,29 +25,34 @@ class TSP:
         self.objf = fitness                # Objective function
 
         self.distanceMatrix = read_from_file(filename)
-        self.numCities = self.distanceMatrix.shape[0]         # Boundary of the domain, not intended to be changed
+        self.numCities = self.distanceMatrix.shape[0]         # Boundary of the domain, not to be changed
+        self.maxTime = 300                                    # Maximum 5 minutes
 
-        # Initialize population
-        self.population = np.zeros((self.lambdaa, self.numCities-1)).astype(int)
+        """ Initialize the first population """
+        self.population = np.zeros((self.lambdaa, self.numCities - 1)).astype(int)
+        # self.population = np.vstack([np.arange(1, self.numCities)] * self.lambdaa)
         for i in range(self.lambdaa):
             self.population[i, :] = self.random_cycle()
+            # np.random.shuffle(self.population[i, :])
 
-    """ The main evolutionary algorithm loop """
     def optimize(self):
-        for i in range(self.numIters):
-            # The evolutionary algorithm
+        startTotTime = time.time()
+        i=0
+        mean = []
+        best = []
+        while ((time.time() - startTotTime) < self.maxTime) and (i<=self.numIters):
             start = time.time()
 
             startselection = time.time()
-            selected = self.selection(self.population, self.k)                  # selected = initial*2
+            selected = self.selection(self.population, self.k)                          # selected = initial*2
             selectiontime = time.time() - startselection
 
             startcross = time.time()
-            offspring = self.pmx_crossover(selected, self.k)                        # offspring = initial
+            offspring = self.pmx_crossover(selected, self.k)                            # offspring = initial
             crossstime = time.time() - startcross
 
             startmutate = time.time()
-            joinedPopulation = np.vstack((self.mutate(offspring), self.population))    # joinedPopulation = polutation + mutated children = lambdaa*2
+            joinedPopulation = np.vstack((self.mutate(offspring), self.population))     # joinedPopulation = polutation + mutated children = lambdaa*2
             mutatetime = time.time() - startmutate
 
             startelemination = time.time()
@@ -54,66 +60,62 @@ class TSP:
             elimtime = time.time() - startelemination
 
             itT = time.time() - start
-
-            # Show progress
-            # fvals = np.array([self.objf(path) for path in self.population])
             fvals = pop_fitness(self.population, self.distanceMatrix)
-            meanObj = np.mean(fvals)
-            bestObj = np.min(fvals)
-            print(f'{i + 1}) {itT: .2f}s:\t Mean fitness = {meanObj: .5f} \t Best fitness = {bestObj: .5f} \t pop shape = {tsp.population.shape} \t select: {selectiontime : .3f}s, cross: {crossstime: .3f}s, mutate: {mutatetime: .3f}s, eliminate: {elimtime: .3f}s')
+            mean.append(np.mean(fvals))
+            best.append(np.min(fvals))
+            print(f'{i}) {itT: .2f}s:\t Mean fitness = {mean[i]: .5f} \t Best fitness = {best[i]: .5f}\t pop shape = {tsp.population.shape}\t selection = {selectiontime : .2f}s, cross = {crossstime: .2f}s, mutate = {mutatetime: .2f}s')
+            i=i+1
         print('Done')
+        totTime = time.time() - startTotTime
+        print(f'Tot time: {totTime: .2f}s')
+        return mean, best, i-1
 
+    """ Perform k-tournament selection to select pairs of parents. """
     def selection_kTour(self, population, k):
         randIndices = random.choices(range(np.size(population,0)), k = k)
         best = np.argmin(pop_fitness(population[randIndices, :], self.distanceMatrix))
         return population[randIndices[best], :]
-
-    """ Perform k-tournament selection to select pairs of parents. """
+    
     def selection(self, population, k):
         selected = np.zeros((self.mu, self.numCities - 1)).astype(int)
         for i in range(self.mu):
             selected[i, :] = self.selection_kTour(population, k)
         return selected
 
+    """ Perform recombination operators on the population """
     def crossover(self, population, k):
-        """Keeps the longest common subpath of two good parents and randomly connects the rest."""
-        offspring = np.zeros((self.lambdaa, 2)).astype(int)
+        offspring = np.zeros((self.lambdaa, self.numCities - 1)).astype(int)
         for i in range(self.lambdaa):
-            # 1. find 2 good parents with k-tour
             p1 = self.selection_kTour(population, k)
-            p2 = self.selection_kTour(population, k)
-            # 2. find their common longest subpath
+            p2 = self.selection_kTour(population, k) 
             subpath = Utilis.longest_common_subpath(p1, p2)
-            # 3. expand it until the cycle is closed, using DFS
-            goal = subpath[0]
-            frontier = [(subpath[-1], subpath)]
-            expanded = set(subpath)
-            offspring[i, :] = self.random_cycle(goal, frontier, expanded)
+            restPath = np.setdiff1d(p1, subpath)
+            np.random.shuffle(restPath)
+            offspring[i, :] = np.append(subpath, restPath)
         return offspring
-
 
     def pmx_crossover(self, selected, k):
         offspring = np.zeros((self.lambdaa, self.numCities - 1)).astype(int)
-        parents = list(zip(selected[::2], selected[1::2]))
+        parents = list(zip(selected[::2], selected[1::2]))      # why?
         for p in range(len(parents)):
             p1, p2 = parents[p][0], parents[p][1]
             cp1, cp2 = sorted(random.sample(range(len(p1)), 2))  # random crossover points
             seg_p1, seg_p2 = p1[cp1:cp2 + 1], p2[cp1:cp2 + 1]
             child = np.zeros(len(p1))
-            child[cp1:cp2 + 1] = seg_p1
+            child[cp1:cp2 + 1] = seg_p1                          # copy randomly selected segment from first parent
             for ii in range(len(seg_p2)):
                 i = k = seg_p2[ii]
                 if i not in seg_p1:
                     j = seg_p1[ii]
                     assigned = False
-                    while not assigned:
+                    while not assigned:                         # why?
                         ind_j2 = np.where(p2 == j)[0]
                         if ind_j2[0] < cp1 or cp2 < ind_j2[0]:
                             child[ind_j2] = i
                             assigned = True
                         else:
-                            k = p2[ind_j2[0]]
-                            j = p1[ind_j2[0]]
+                            k = p2[ind_j2[0]]                   # useless?
+                            j = p1[ind_j2[0]]                   # useless?
             child[child == 0] = p2[child == 0]
             offspring[p] = child
         return offspring
@@ -122,12 +124,12 @@ class TSP:
     """ Perform mutation, adding a random Gaussian perturbation. """
     def mutation(self, offspring, alpha):
         i = np.where(np.random.rand(np.size(offspring, 0)) <= alpha)[0]
-        offspring[i, :] = self.random_cycle()
+        np.random.shuffle(offspring[i,:])
         return offspring
 
     def mutate(self, offspring):
         for i in range(len(offspring)):
-            if random.random() <= self.alpha:  # Check if the random value is less than or equal to alpha
+            if random.random() <= self.alpha:   # Check if the random value is less than or equal to alpha
                 mutation_type = random.choices(
                     [self.mutation_swap, self.mutation_insert, self.mutation_scramble, self.mutation_inversion],
                     self.mutationratios)[0]
@@ -173,7 +175,6 @@ class TSP:
             # The items of the stack are (node, goal-to-node-path) tuples.
             frontier = [(goal, [goal])]
             expanded = set()
-
         while frontier:
             u, path = frontier.pop()
             if u == goal:
@@ -184,14 +185,12 @@ class TSP:
             if u in expanded:
                 continue
             expanded.add(u)
-
             # loop through the neighbours at a random order, to result to order in the frontier
             np.random.shuffle(nodes)
             for v in nodes:
                 if (v != u) and (self.distanceMatrix[u][v] != np.inf):
                     # this is a neighbour
                     frontier.append((v, path + [v]))
-
         # in case it got to a dead end, rerun
         return self.random_cycle()
 
@@ -205,37 +204,29 @@ def fitness(path, distanceMatrix):
         sum += distanceMatrix[path[i]][path[i + 1]]
     return sum
 
+def plot_graph(mean, best):
+    plt.plot(mean, label='Mean fitness')
+    plt.title('Mean fitness convergence')
+    plt.xlabel('Iterations')
+    plt.ylabel('mean fitness')
 
-# Testing selection
-# pop = tsp.selection(tsp.population, 5)
-# print(pop.shape)
-# print(np.unique(pop, axis=0).shape)
+    plt.plot(best, label='Best fitness')
+    plt.title('Best fitness convergence')
+    plt.xlabel('Iterations')
+    plt.ylabel('best fitness')
+    plt.legend()
+    plt.show()
 
-# Testing mutation
-# np.set_printoptions(threshold=np.inf)
-# pop1 = tsp.population
-# print(pop1)
-# pop2  = tsp.mutation(tsp.population, tsp.alpha)
-# print(pop2)
-
-# Testing crossover
-# pop1 = tsp.population
-# pop2 = tsp.crossover(pop1, tsp.k)
-# print(pop2)
-# print(pop2.shape)
-
-# Testing elimination
-# pop2 = tsp.elimination(pop1, tsp.lambdaa)
-# print(pop1.shape)
-# print(pop2.shape)
-# mean_fitness1 = sum(pop_fitness(pop1, tsp.distanceMatrix)) / len(pop1)
-# print(mean_fitness1)
-# pop2 = tsp.elimination(pop1, tsp.lambdaa)
-# mean_fitness2 = sum(pop_fitness(pop2, tsp.distanceMatrix)) / len(pop2)
-# print(mean_fitness2)
+# --------------------------------------------------------- #
 
 tsp = TSP(fitness, "../data/tour50.csv")
-tsp.optimize()
+mean, best, it = tsp.optimize()
+plot_graph(mean, best)
+
+
+
+
+
 
 
 
